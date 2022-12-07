b3204c4951222036cb2b5a48181b47dc9a60f544
Carol <acrla@academico.ufpb.br>
2022-12-07 09:08:22 -0300

    Implemetacao insercao ABP

--- src/ABP.java
+++ src/ABP.java
@@ -36,31 +36,37 @@ public class ABP {
 		//return null;
 	}
 	
-	public NoABP buscaIterativa(int valor) {          
+	public int buscaIterativa(int valor) {          
+		String ordem = "";
+		int pos = 0;
+
 		if (vazia())
-			return null;
+			return -1;
 		
 		NoABP aux = raiz;
 		while (aux != null) {
 			// Verificando se o conteudo do no atual 
 			// é igual ao valor procurado
 			if (aux.getConteudo() == valor) {
-				return aux;
+				pos = posicoes(ordem);
+				return pos;
 			}
 			
 			// Se o valor procurado for menor que raiz,
 			// continue pesquisando na sub-arv da esq.
 			if (valor < aux.getConteudo()){
 				aux = aux.getEsq();
+				ordem += "0";
 			}
 			// Caso contratio, pesquise na sub-arv 
 			// da direita
 			else {
 				aux = aux.getDir();
+				ordem += "1";
 			}
 		}
 
-		return null;
+		return -1;
 	}
 	
 	
@@ -101,17 +107,20 @@ public class ABP {
 	/**Insere um nó em uma árvore ABP
 	    Retorna 1 se a inserção for com sucesso.
 	    Caso contrário retorna 0*/
-	public boolean insere(int valor){
+	public int insere(int valor) {
 
 		NoABP novoNo = new NoABP();
 		novoNo.setConteudo(valor);
 		novoNo.setEsq(null);
 		novoNo.setDir(null);
 
+		String seq = "";
+		int pos;
+
 		// Quando a arvore estiver vazia, o novoNó será a raiz da arv
 		if (raiz == null){ 
 	 		raiz = novoNo;
-			return true;
+			return 8;
 		}
 
 	    // Procura a posicao correta pra inserir o novo no
@@ -119,17 +128,121 @@ public class ABP {
 	    NoABP p = null;
 	    while (aux != null) {
 	    	p = aux;
-			if (valor < aux.getConteudo())
+			if (valor < aux.getConteudo()) {
 				aux = aux.getEsq();
-			else
+				seq += "0";
+			} else {
 				aux = aux.getDir();
+				seq += "1";
+			}
 		}
 
-		// Encontrou um nó folha para inserir
-		if (valor < p.getConteudo())
-			p.setEsq(novoNo);
+		pos = posicoes(seq);
+
+		if(pos == -1) {
+			return pos;
+		} else {
+			// Encontrou um nó folha para inserir
+			if (valor < p.getConteudo()) {
+				p.setEsq(novoNo);
+			} else {
+				p.setDir(novoNo);
+			}
+		}
+		
+		return pos;
+	}
+
+	private int posicoes(String seq) {
+		int posicao = 0;
+
+		if(seq.equals("0")) { posicao = 4; } 
+		else if (seq.equals("00")) { posicao = 2; }
+		else if (seq.equals("000")) { posicao = 1; }
+		else if (seq.equals("001")) { posicao = 3; }
+		else if (seq.equals("01")) { posicao = 6; }
+		else if (seq.equals("010")) { posicao = 5; }
+		else if (seq.equals("011")) { posicao = 7; }
+		else if (seq.equals("1")) { posicao = 12; }
+		else if (seq.equals("10")) { posicao = 10; }
+		else if (seq.equals("100")) { posicao = 9; }
+		else if (seq.equals("101")) { posicao = 11; }
+		else if (seq.equals("11")) { posicao = 14; }
+		else if (seq.equals("110")) { posicao = 13; }
+		else if (seq.equals("111")) { posicao = 15; }
+		else { posicao = -1; }
+		
+		return posicao;
+	}
+
+	/** Exibe o conteúdo de uma árvore em pré-ordem*/
+	private void exibePreOrdem(NoABP T) {
+		if (T == null)
+			return;
+	
+		// Imprime a raiz
+		System.out.print(" " + T.getConteudo());
+		
+		// Caminha recursivamente na sub-arv da esq (pre-ordem)
+		if (T.getEsq() != null)
+			exibePreOrdem(T.getEsq());
+	
+		// Caminha recursivamente na sub-arv da dir (pre-ordem)
+	 	if (T.getDir() != null)
+			exibePreOrdem(T.getDir());
+	}
+
+	/** Exibe o conteúdo de uma árvore em pré-ordem*/
+	public void exibePreOrdem() {
+		if (raiz == null)
+			System.out.println("Arvore vazia");
 		else
-			p.setDir(novoNo);
-		return true;
+			exibePreOrdem(raiz);
+	}	
+	
+	
+	/** Exibe o conteúdo de uma árvore em pré-ordem*/
+	private void exibeInOrdem(NoABP T) {
+		if (T == null)
+			return ;
+
+		if (T.getEsq() != null)
+			exibeInOrdem(T.getEsq());
+	
+	    System.out.print(" " + T.getConteudo());
+	
+	 	if (T.getDir() != null)
+			exibeInOrdem(T.getDir());
+	}
+
+	
+	/** Exibe o conteúdo de uma árvore em pré-ordem*/
+	public void exibeInOrdem() {
+		if (raiz == null)
+			System.out.println("Arvore vazia");
+		else
+			exibeInOrdem(raiz);
+	}	
+	
+	/** Exibe o conteúdo de uma árvore em pré-ordem*/
+	private void exibePosOrdem(NoABP T) {
+		if (T == null)
+			return ;
+
+		if (T.getEsq() != null)
+			exibePosOrdem(T.getEsq());
+	
+	 	if (T.getDir() != null)
+			exibePosOrdem(T.getDir());
+
+	 	System.out.print(" " + T.getConteudo());
+	}
+	
+	/** Exibe o conteúdo de uma árvore em pré-ordem*/
+	public void exibePosOrdem() {
+		if (raiz == null)
+			System.out.println("Arvore vazia");
+		else
+			exibePosOrdem(raiz);
 	}
 }
--- src/ABP_GUI.java
+++ src/ABP_GUI.java
@@ -10,15 +10,17 @@ public class ABP_GUI extends JDialog {
     private JPanel contentPanel = new JPanel();
     private ABP abp = new ABP();
 
+    private int indice;
     private int valor;
-    private int pos;
 
     private JLabel elementos[] = new JLabel[15];
-    private JLabel linhas[] = new JLabel[14];
+    private JLabel linhas[] = new JLabel[15];
 
     public ABP_GUI() {
         paintCabecalho();
         infoPanel();
+
+        indice = 0; // inicializar em limpar()
 
         // paintElem(1, 1);
         // paintLinha(1, 0);
@@ -65,17 +67,17 @@ public class ABP_GUI extends JDialog {
         contentPanel.setBounds(0, 0, 1000, 600);
         contentPanel.setVisible(true);
 
-        // ImageIcon iconABP = new ImageIcon("imagens/diagram2.png");
-        // iconABP = scaleImage(iconABP, 85, 85);
+        ImageIcon iconABP = new ImageIcon("imagens/diagram2.png");
+        iconABP = scaleImage(iconABP, 85, 85);
         
-        // JLabel l_icon = new JLabel();
-        // l_icon.setForeground(new Color(34, 38, 45));
-		// l_icon.setFont(new Font("Courier", Font.BOLD, 25));
-        // l_icon.setIcon(iconABP);
-        // // l_icon.setBounds(880, 475, 700, 80);
+        JLabel l_icon = new JLabel();
+        l_icon.setForeground(new Color(34, 38, 45));
+		l_icon.setFont(new Font("Courier", Font.BOLD, 25));
+        l_icon.setIcon(iconABP);
+        l_icon.setBounds(870, 462, 100, 80);
         // l_icon.setBounds(30, 55, 100, 80);
-        // l_icon.setVisible(true);
-        // this.getContentPane().add(l_icon);
+        l_icon.setVisible(true);
+        this.getContentPane().add(l_icon);
         
         JLabel titulo = new JLabel("ÁRVORE BINÁRIA DE PESQUISA");
         titulo.setForeground(new Color(34, 38, 45));
@@ -98,7 +100,7 @@ public class ABP_GUI extends JDialog {
         this.getContentPane().add(b_limpar);
         b_limpar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // limpar();
+                // limpar(); // limpar indice
                 // atualizaInfoPanel();
             }
         });
@@ -131,7 +133,7 @@ public class ABP_GUI extends JDialog {
         this.getContentPane().add(info_text);
         
         JLabel info = new JLabel();
-        info.setBounds(30, 452, 300, 90);
+        info.setBounds(30, 452, 380, 90);
         info.setBackground(Color.LIGHT_GRAY);
         info.setOpaque(true);
         this.getContentPane().add(info);
@@ -145,19 +147,19 @@ public class ABP_GUI extends JDialog {
         JButton b_inserir = new JButton("Inserir");
         b_inserir.setForeground(new Color(34, 38, 45));
         b_inserir.setBackground(Color.WHITE);
-        b_inserir.setBounds(360, 460, 120, 30);
+        b_inserir.setBounds(440, 460, 120, 30);
         b_inserir.setFont(new Font("Courier", Font.PLAIN, 18));
         b_inserir.setFocusable(false);
         this.getContentPane().add(b_inserir);
         
         JLabel l_inserir = new JLabel();
-        l_inserir.setBounds(380, 500, 160, 35);
+        l_inserir.setBounds(460, 500, 160, 35);
         l_inserir.setBorder(BorderFactory.createLoweredBevelBorder());
         l_inserir.setOpaque(false);
         this.getContentPane().add(l_inserir);
         
         JLabel t_inserir = new JLabel("<html> <p>Elemento: </p> </html>");
-        t_inserir.setBounds(390, 500, 140, 35);
+        t_inserir.setBounds(470, 500, 140, 35);
         t_inserir.setFont(new Font("Courier", Font.PLAIN, 18));
         t_inserir.setForeground(new Color(34, 38, 45));
         t_inserir.setHorizontalAlignment(SwingConstants.LEADING);
@@ -165,7 +167,7 @@ public class ABP_GUI extends JDialog {
         this.getContentPane().add(t_inserir);
         
         JTextArea i_elemento = new JTextArea();
-        i_elemento.setBounds(500, 509, 30, 18);
+        i_elemento.setBounds(580, 509, 30, 18);
         i_elemento.setAlignmentX(CENTER_ALIGNMENT);
         this.getContentPane().add(i_elemento);
 
@@ -179,7 +181,6 @@ public class ABP_GUI extends JDialog {
                     } else if(abp.busca(valor) == null) {
                         inserir(valor);
                     } else if(abp.busca(valor) != null) {
-                        System.out.println("Elem " + abp.busca(valor).getConteudo());
                         JOptionPane.showMessageDialog(null, "Valor " + i_txtValor + " já inserido na árvore", "[ERRO] INSERÇÃO INVÁLIDA", JOptionPane.WARNING_MESSAGE);
                     }
                 } catch(Exception e1) {
@@ -190,71 +191,23 @@ public class ABP_GUI extends JDialog {
             }
         });
         
-        // REMOVER
-        JButton b_remover = new JButton("Remover");
-        b_remover.setForeground(new Color(34, 38, 45));
-        b_remover.setBackground(Color.WHITE);
-        b_remover.setBounds(560, 460, 120, 30);
-        b_remover.setFont(new Font("Courier", Font.PLAIN, 18));
-        b_remover.setFocusable(false);
-        this.getContentPane().add(b_remover);
-        
-        JLabel l_remover = new JLabel();
-        l_remover.setBounds(580, 500, 160, 35);
-        l_remover.setBorder(BorderFactory.createLoweredBevelBorder());
-        l_remover.setOpaque(false);
-        this.getContentPane().add(l_remover);
-        
-        JLabel t_remover = new JLabel("<html> <p>Elemento: </p> </html>");
-        t_remover.setBounds(590, 500, 140, 35);
-        t_remover.setFont(new Font("Courier", Font.PLAIN, 18));
-        t_remover.setForeground(new Color(34, 38, 45));
-        t_remover.setHorizontalAlignment(SwingConstants.LEADING);
-        t_remover.setVerticalAlignment(SwingConstants.CENTER);
-        this.getContentPane().add(t_remover);
-        
-        JTextArea r_elemento = new JTextArea();
-        r_elemento.setBounds(700, 509, 30, 18);
-        r_elemento.setAlignmentX(CENTER_ALIGNMENT);
-        this.getContentPane().add(r_elemento);
-    
-        b_remover.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String r_txtElem = r_elemento.getText();
-                try {
-                    if(r_txtElem.equals("")) {
-                        JOptionPane.showMessageDialog(null, "Preencha o campo Elemento", "[ERRO] CAMPO VAZIO", JOptionPane.WARNING_MESSAGE);
-                    }
-                    if(!r_txtElem.equals("")) {
-                        valor = Integer.parseInt(r_txtElem);
-                        remover(valor);
-                    }
-                } catch(Exception e1) {
-                    JOptionPane.showMessageDialog(null, "ERRO", "", JOptionPane.WARNING_MESSAGE);
-                    System.out.println("erro " + e1);
-                }
-                r_elemento.setText("");
-                // atualizaInfoPanel();
-            }
-        });
-        
         // BUSCAR
         JButton b_buscar = new JButton("Buscar");
         b_buscar.setForeground(new Color(34, 38, 45));
         b_buscar.setBackground(Color.WHITE);
-        b_buscar.setBounds(760, 460, 120, 30);
+        b_buscar.setBounds(650, 460, 120, 30);
         b_buscar.setFont(new Font("Courier", Font.PLAIN, 18));
         b_buscar.setFocusable(false);
         this.getContentPane().add(b_buscar);
         
         JLabel l_buscar = new JLabel();
-        l_buscar.setBounds(780, 500, 160, 35);
+        l_buscar.setBounds(670, 500, 160, 35);
         l_buscar.setBorder(BorderFactory.createLoweredBevelBorder());
         l_buscar.setOpaque(false);
         this.getContentPane().add(l_buscar);
         
         JLabel t_buscar = new JLabel("<html> <p>Elemento: </p> </html>");
-        t_buscar.setBounds(790, 500, 140, 35);
+        t_buscar.setBounds(680, 500, 140, 35);
         t_buscar.setFont(new Font("Courier", Font.PLAIN, 18));
         t_buscar.setForeground(new Color(34, 38, 45));
         t_buscar.setHorizontalAlignment(SwingConstants.LEADING);
@@ -262,117 +215,138 @@ public class ABP_GUI extends JDialog {
         this.getContentPane().add(t_buscar);
         
         JTextArea b_elemento = new JTextArea();
-        b_elemento.setBounds(900, 509, 30, 18);
+        b_elemento.setBounds(790, 509, 30, 18);
         b_elemento.setAlignmentX(CENTER_ALIGNMENT);
         this.getContentPane().add(b_elemento);
         
         b_buscar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String r_txtElem = r_elemento.getText();
+                String b_txtElem = b_elemento.getText();
                 try {
-                    if(r_txtElem.equals("")) {
+                    if(b_txtElem.equals("")) {
                         JOptionPane.showMessageDialog(null, "Preencha o campo Elemento", "[ERRO] CAMPO VAZIO", JOptionPane.WARNING_MESSAGE);
                     }
-                    if(!r_txtElem.equals("")) {
-                        valor = Integer.parseInt(r_txtElem);
-                        remover(valor);
+                    if(!b_txtElem.equals("")) {
+                        valor = Integer.parseInt(b_txtElem);
+                        buscar(b_txtElem, valor);
                     }
                 } catch(Exception e1) {
                     JOptionPane.showMessageDialog(null, "ERRO", "", JOptionPane.WARNING_MESSAGE);
                     System.out.println("erro " + e1);
                 }
-                r_elemento.setText("");
+                b_elemento.setText("");
             }
         });
     }
     
     private void inserir(int valor) {
-        if(abp.vazia()) {
-            pos = 1;
+        int pos;
+
+        pos = abp.insere(valor);
+
+        if(pos == -1) {
+            JOptionPane.showMessageDialog(null, "Não é possível adicionar o elemento " + valor, "[ERRO] PROFUNDIDADE MÁXIMA ATINGIDA", JOptionPane.WARNING_MESSAGE);
+        } else if (pos == 8) {
             paintElem(pos, valor);
         } else {
-            pos += 1;
-            paintLinha(pos-1, 0);
-            paintLinha(pos-1, 1);
+            paintLinha(pos);
             paintElem(pos, valor);
         }
-        
-        abp.insere(valor);
-    }
 
-    private void remover(int valor) {
-        pos -= 1;
+        indice++;
     }
     
-    private void buscar(int valor, int pos) {
-        
+    private void buscar(String texto, int valor) {
+        if(abp.vazia()) {
+            JOptionPane.showMessageDialog(null, "Não há elementos na árvore", "[ERRO] ÁRVORE VAZIA", JOptionPane.WARNING_MESSAGE);
+        } else {
+            int pos = abp.buscaIterativa(valor);
+
+            if(pos == -1) {
+                JOptionPane.showMessageDialog(null, "Elemento não inserido na árvore", "[ERRO] ELEMENTO INEXISTENTE", JOptionPane.WARNING_MESSAGE);
+            } else {
+                for(int i = 0; i < indice; i++) {
+                    elementos[i].setBackground(Color.GREEN);
+                    if(elementos[i].getText().equals(texto)) {
+                        break;
+                    }
+                    try {
+                        Thread.sleep(1000); // so atualiza a janela ao fim do evento
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                    if(i == 0) {
+                        elementos[0].setBackground(Color.LIGHT_GRAY);
+                    } else {
+                        elementos[i].setBackground(Color.WHITE);
+                    }
+                }
+            }
+
+        }
     }
 
     private void paintElem(int pos, int valor) {
-        // int lin = 1;
         int x = 0, y = 0;
 
-        if(pos == 1) { x = 450; y = 70;  }
-        if(pos == 2) { x = 220; y = 180; }
-        if(pos == 3) { x = 680; y = 180; }
-        if(pos == 4) { x = 105; y = 280; }
-        if(pos == 5) { x = 335; y = 280; }
-        if(pos == 6) { x = 565; y = 280; }
-        if(pos == 7) { x = 795; y = 280; }
-        if(pos == 8) { x = 30;  y = 370; }
-        if(pos == 9) { x = 150; y = 370; }
-        if(pos == 10) { x = 270; y = 370; }
-        if(pos == 11) { x = 390; y = 370; }
-        if(pos == 12) { x = 510; y = 370; }
-        if(pos == 13) { x = 630; y = 370; }
-        if(pos == 14) { x = 750; y = 370; }
+        if(pos == 8) { x = 450; y = 70;  }
+        if(pos == 4) { x = 220; y = 180; }
+        if(pos == 12) { x = 680; y = 180; }
+        if(pos == 2) { x = 105; y = 280; }
+        if(pos == 6) { x = 335; y = 280; }
+        if(pos == 10) { x = 565; y = 280; }
+        if(pos == 14) { x = 795; y = 280; }
+        if(pos == 1) { x = 30;  y = 370; }
+        if(pos == 3) { x = 150; y = 370; }
+        if(pos == 5) { x = 270; y = 370; }
+        if(pos == 7) { x = 390; y = 370; }
+        if(pos == 9) { x = 510; y = 370; }
+        if(pos == 11) { x = 630; y = 370; }
+        if(pos == 13) { x = 750; y = 370; }
         if(pos == 15) { x = 870; y = 370; }
         
         JLabel elem =  new JLabel("" + valor);
-        elementos[pos-1] = elem;
-        contentPanel.add(elementos[pos-1]);
-        elementos[pos-1].setBounds(x, y, 80, 60);
-        elementos[pos-1].setFont(new Font("Courier", Font.BOLD, 25));
-        elementos[pos-1].setForeground(new Color(34, 38, 45));
-        elementos[pos-1].setBackground(Color.WHITE);
+        elementos[indice] = elem;
+        contentPanel.add(elementos[indice]);
+        elementos[indice].setBounds(x, y, 80, 60);
+        elementos[indice].setFont(new Font("Courier", Font.BOLD, 25));
+        elementos[indice].setForeground(new Color(34, 38, 45));
+        elementos[indice].setBackground(Color.WHITE);
         elementos[0].setBackground(Color.LIGHT_GRAY);
-        elementos[pos-1].setBorder(BorderFactory.createLineBorder(new Color(34, 38, 45)));
-        elementos[pos-1].setHorizontalAlignment(SwingConstants.CENTER);
-        elementos[pos-1].setOpaque(true);
-        elementos[pos-1].setVisible(true);
+        elementos[indice].setBorder(BorderFactory.createLineBorder(new Color(34, 38, 45)));
+        elementos[indice].setHorizontalAlignment(SwingConstants.CENTER);
+        elementos[indice].setOpaque(true);
+        elementos[indice].setVisible(true);
     }
     
-    private void paintLinha(int pos, int filho) {
-        // int lin = 1;
+    private void paintLinha(int pos) {
         int x = 0, y = 0;
         int w = 0, h = 0;
         String path = "";
         
-        if(filho == 0) {
-            if(pos == 1) { x = 260; y = 130; path = "imagens/Line1_esq.png"; w = 231; h = 51; }
-            if(pos == 2) { x = 145; y = 240; path = "imagens/Line2_esq.png"; w = 116; h = 41; } 
-            if(pos == 3) { x = 605; y = 240; path = "imagens/Line2_esq.png"; w = 116; h = 41; } 
-            if(pos == 4) { x = 70; y = 340; path = "imagens/Line4_esq.png"; w = 76; h = 31; } 
-            if(pos == 5) { x = 310; y = 340; path = "imagens/Line5_esq.png"; w = 66; h = 31; } 
-            if(pos == 6) { x = 550; y = 340; path = "imagens/Line6_esq.png"; w = 56; h = 31; } 
-            if(pos == 7) { x = 790; y = 340; path = "imagens/Line7_esq.png"; w = 47; h = 31; }
-        } else if (filho == 1) {
-            if(pos == 1) { x = 490; y = 130; path = "imagens/Line1_dir.png"; w = 231; h = 51; }
-            if(pos == 2) { x = 260; y = 240; path = "imagens/Line2_dir.png"; w = 116; h = 41; } 
-            if(pos == 3) { x = 720; y = 240; path = "imagens/Line2_dir.png"; w = 116; h = 41; } 
-            if(pos == 4) { x = 145; y = 340; path = "imagens/Line4_dir.png"; w = 47; h = 31; } 
-            if(pos == 5) { x = 375; y = 340; path = "imagens/Line5_dir.png"; w = 56; h = 31; } 
-            if(pos == 6) { x = 605; y = 340; path = "imagens/Line6_dir.png"; w = 66; h = 31; } 
-            if(pos == 7) { x = 835; y = 340; path = "imagens/Line7_dir.png"; w = 76; h = 31; }
-        }
+        if(pos == 4) { x = 260; y = 130; path = "imagens/Line1_esq.png"; w = 231; h = 51; }
+        if(pos == 2) { x = 145; y = 240; path = "imagens/Line2_esq.png"; w = 116; h = 41; } 
+        if(pos == 10) { x = 605; y = 240; path = "imagens/Line2_esq.png"; w = 116; h = 41; } 
+        if(pos == 1) { x = 70; y = 340; path = "imagens/Line4_esq.png"; w = 76; h = 31; } 
+        if(pos == 5) { x = 310; y = 340; path = "imagens/Line5_esq.png"; w = 66; h = 31; } 
+        if(pos == 9) { x = 550; y = 340; path = "imagens/Line6_esq.png"; w = 56; h = 31; } 
+        if(pos == 13) { x = 790; y = 340; path = "imagens/Line7_esq.png"; w = 47; h = 31; }
+        if(pos == 12) { x = 490; y = 130; path = "imagens/Line1_dir.png"; w = 231; h = 51; }
+        if(pos == 6) { x = 260; y = 240; path = "imagens/Line2_dir.png"; w = 116; h = 41; } 
+        if(pos == 14) { x = 720; y = 240; path = "imagens/Line2_dir.png"; w = 116; h = 41; } 
+        if(pos == 3) { x = 145; y = 340; path = "imagens/Line4_dir.png"; w = 47; h = 31; } 
+        if(pos == 7) { x = 375; y = 340; path = "imagens/Line5_dir.png"; w = 56; h = 31; } 
+        if(pos == 11) { x = 605; y = 340; path = "imagens/Line6_dir.png"; w = 66; h = 31; } 
+        if(pos == 15) { x = 835; y = 340; path = "imagens/Line7_dir.png"; w = 76; h = 31; }
         
         ImageIcon icon_linha = new ImageIcon(path);
         JLabel l_linha = new JLabel();
-        linhas[pos-1] = l_linha;
-        contentPanel.add(linhas[pos-1]);
-        linhas[pos-1].setIcon(icon_linha);
-        linhas[pos-1].setBounds(x, y, w, h);
-        linhas[pos-1].setVisible(true);
+        linhas[indice-1] = l_linha;
+        contentPanel.add(linhas[indice-1]);
+        linhas[indice-1].setIcon(icon_linha);
+        linhas[indice-1].setBounds(x, y, w, h);
+        linhas[indice-1].setVisible(true);
     }
     
     private ImageIcon scaleImage(ImageIcon icon, int w, int h) {
@@ -391,7 +365,7 @@ public class ABP_GUI extends JDialog {
 
         return new ImageIcon(icon.getImage().getScaledInstance(nw, nh, Image.SCALE_DEFAULT));
     }
-
+    
     public static void main(String[] args) {
         new ABP_GUI();
     }
